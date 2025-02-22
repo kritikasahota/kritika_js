b382980325d2a81574db1ee024ca946a2f78543d
Kritikasahota <sahotakritika029@gmail.com>
2025-02-22 23:22:20 +0530

    adding files

---
+++ module1/strings.js
@@ -0,0 +1,41 @@
+// STRING DECLARATION
+let str ="single-quoted";
+let str2 = "double-quoted";
+
+// STRING LENGTH
+console.log(str.length);
+
+// STRING INDICES
+console.log(str[5]);
+
+// TEMPLATE LITERALS
+let item = "pen" ;
+let price = 10;
+console.log(`The cost of ${item} is ${price} rupees`)
+
+// STRING METHODS
+let a = "Kritika  ";
+let b = "Sahota ";
+
+console.log(a.toUpperCase());
+
+console.log(a.toLowerCase());
+
+console.log(a.trim());
+
+console.log(a.slice(2));
+console.log(a.slice(0,5));
+console.log(a.slice(-4,-1));
+
+console.log(a.concat(b));
+
+console.log(a.replace("ka","22"));
+
+console.log(a.charAt(2));
+
+
+
+
+
+
+
