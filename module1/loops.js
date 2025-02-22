0f0150d9c91bf65245ed853b0caa2f54980c84ee
Kritikasahota <sahotakritika029@gmail.com>
2025-02-22 23:24:30 +0530

    updated

--- module1/loops.js
+++ module1/loops.js
@@ -1,33 +1,32 @@
-// // FOR LOOP
-// for(let count = 0; count <= 5; count++){
-//     console.log("HELLO");
-// }
-// console.log("loop has ended");
+// FOR LOOP
+for(let count = 0; count <= 5; count++){
+    console.log("HELLO");
+}
+console.log("loop has ended");
 
-// // sum of n numbers using for loop
-// let sum = 0;
-// let n = 7;
-// for(let i = 0; i <= n; i++) {
-//     sum = sum + i;
-//     console.log("sum =  ",sum);
-// }
+// sum of n numbers using for loop
+let sum = 0;
+let n = 7;
+for(let i = 0; i <= n; i++) {
+    sum = sum + i;
+    console.log("sum =  ",sum);
+}
 
-// // print 1 to 5
-// for(let a = 1; a <= 5; a++){
-//     console.log("a = ", a);
-// }
+// print 1 to 5
+for(let a = 1; a <= 5; a++){
+    console.log("a = ", a);
+}
 
-// // WHILE LOOP
-// let b = 0;
-// while(b <= 5){
-//     console.log("b = ",b);
-//     b++;
-// }
+// WHILE LOOP
+let b = 0;
+while(b <= 5){
+    console.log("b = ",b);
+    b++;
+}
 
-
- // TASK 1 -- print even numbers upto 100
- for( let a = 0; a <= 100; a++) {
-      if(a % 2 == 0) {
-        console.log(a);
-      }
- }
+// TASK 1 -- print even numbers upto 100
+for( let a = 0; a <= 100; a++) {
+   if(a % 2 == 0) {
+     console.log(a);
+    }
+}
