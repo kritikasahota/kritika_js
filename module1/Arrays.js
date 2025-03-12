6e80fcb0b879d924f8beee931128104501f3e843
Kritikasahota <sahotakritika029@gmail.com>
2025-03-12 22:28:31 +0530

    new code

---
+++ module1/Arrays.js
@@ -0,0 +1,17 @@
+let arr = new Array();
+let arr1 = [];
+let fruits = ["Apple", "Orange", "Plum"];
+console.log(fruits[0]); 
+console.log(fruits[1]);
+
+fruits[2] = 'pear';   /*element canbe replaced*/
+
+fruits[3]= "lemon";    /*adding a new element*/
+console.log(fruits);
+
+console.log(fruits.length);   /*TO FIND LENGTH*/
+
+console.log(fruits[2]);   /*accesing characters*/
+
+console.log( fruits[fruits.length-1] ); /*accessing last element*/
+console.log(fruits.at(-1));
