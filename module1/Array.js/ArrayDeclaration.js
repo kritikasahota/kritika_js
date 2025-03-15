0cc38bd6f01ce477ac03ec2c6f62a37a329fe8e8
Kritikasahota <sahotakritika029@gmail.com>
2025-03-15 14:36:28 +0530

    updated

---
+++ module1/Array.js/ArrayDeclaration.js
@@ -0,0 +1,3 @@
+let arr = new Array();
+let arr1 = [];
+let fruits = ["Apple", "Orange", "Plum"];
--- module1/Arrays.js
+++ module1/Arrays.js
@@ -4,7 +4,7 @@ let fruits = ["Apple", "Orange", "Plum"];
 console.log(fruits[0]); 
 console.log(fruits[1]);
 
-fruits[2] = 'pear';   /*element canbe replaced*/
+fruits[2] = 'pear';   /*element can be replaced*/
 
 fruits[3]= "lemon";    /*adding a new element*/
 console.log(fruits);
@@ -13,5 +13,13 @@ console.log(fruits.length);   /*TO FIND LENGTH*/
 
 console.log(fruits[2]);   /*accesing characters*/
 
-console.log( fruits[fruits.length-1] ); /*accessing last element*/
+console.log( fruits[fruits.length-1] );       /*accessing last element*/
 console.log(fruits.at(-1));
+
+for (let i = 0; i< fruits.length; i++)     /*LOOPING OF AN ARRAY*/
+{
+    console.log(fruits[i]);
+} 
+
+console.log(fruits.pop())
+
